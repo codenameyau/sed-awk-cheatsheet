19bc4848156e253a8bd711375bef00e6c094cc73
Jorge Yau <jyau01@citymail.cuny.edu>
2016-04-09 10:46:27 -0400

    Added mad useful sed printing commands

--- README.md
+++ README.md
@@ -1,12 +1,34 @@
-# sed-awk lessons
+# sed-awk
 
-Following the tutorials here.
+Follow the tutorials here.
 - http://www.thegeekstuff.com/tag/sed-tips-and-tricks/
 
-```bash
-# List out the columns in table.
-cat table.txt | sed 1d | awk '{ print $2 }'
 
-# Sum the columns in table.
-cat table.txt | sed 1d | awk '{ sum += $2 } END { print sum }'
+### Mad Useful
+
+```bash
+# List out the second column in the table.
+cat text/table.txt | sed 1d | awk '{ print $2 }'
+
+# Sum the columns in the table.
+cat text/table.txt | sed 1d | awk '{ sum += $2 } END { print sum }'
+```
+
+--
+
+```bash
+# Print lines `2` to `5`.
+sed -n '2,5'p text/geek.txt
+
+# Print lines `2` to the last line.
+sed -n '2,$'p text/geek.txt
+
+# Print lines starting with `3` and skipping by `2`.
+sed -n '3~2'p text/geek.txt
+
+# Prints the line matching the pattern and the next '3' lines.
+sed -n '/Sysadmin/,+3p' text/geek.txt
+
+# Prints the lines matching the between the two patterns.
+sed -n '/Hardware/,/Website/p' text/geek.txt
 ```
--- _sed.skills.sh
+++ _sed.skills.sh
@@ -1,1 +1,40 @@
 #!/bin/bash
+
+#####################################################################
+# PRINTING
+#####################################################################
+
+# Print contents of a file.
+sed -n '/fox/p' text/*
+sed -n '/Sysadmin/p' text/geek.txt
+
+# Print a specific line `N`.
+sed -n '3'p text/geek.txt
+
+# Print lines `3` to `5`.
+sed -n '3,5'p text/geek.txt
+
+# Print lines starting with `3` and skipping by `2`.
+sed -n '3~2'p text/geek.txt
+
+# Print the last line.
+sed -n '$'p text/geek.txt
+
+# Print lines `2` to the last line.
+sed -n '2,$'p text/geek.txt
+
+# Print lines matching the pattern until the specified line.
+sed -n '/Sysadmin/,3p' text/geek.txt
+sed -n '/Oracle/,5p' text/geek.txt
+
+# Print lines starting from `3` until it matches the pattern.
+sed -n '3,/Sysadmin/p' text/geek.txt
+
+# Print lines matching the pattern to the last line.
+sed -n '/Website/,$p' text/geek.txt
+
+# Prints the lines matching the pattern and the next '3' lines.
+sed -n '/Sysadmin/,+3p' text/geek.txt
+
+# Prints the lines matching the between the two patterns.
+sed -n '/Hardware/Website/p' text/geek.txt
