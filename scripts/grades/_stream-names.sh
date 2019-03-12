ad5d764da2ed0a1afdf03ef93156da6464178ab9
Jorge Yau <codenameyau@gmail.com>
2019-03-11 23:07:33 -0400

    Remove scripts

--- scripts/grades/_stream-names.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Count the number of lines.
-data_file="../../data/names.csv"
-total_lines=$(wc -l $data_file | awk '{ print $1 }')
-
-# Get 20 random names. Sed substitution is a bottleneck. Parenthesis and string concat example in awk.
-shuf -i 1-$total_lines -n 20 | xargs -i sed -n 's/"//g; {}p;' $data_file | awk -F',' '{ print $1, $2, "(" $4 ")"}'
